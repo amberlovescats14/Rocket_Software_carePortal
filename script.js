c27e12549c599b71cf40b218506b4bf3b5396bad
Amber Jones <amberlovescats14@gmail.com>
2019-05-04 13:14:04 -0500

    amber

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
  const testing = (e) => {
    e.preventDefault()
-   console.log('hello')
+   let firstName = document.getElementById('firstname').value
+   let ul = document.getElementById('list')
+
  }
  document.getElementById('submit').addEventListener('click', testing)
