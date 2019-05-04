9650a96f89e123be79b2d1beb035112e1c359efe
jjoll <jjoll@me.com>
2019-05-04 14:05:51 -0500

    pull

--- script.js
+++ script.js
@@ -21,18 +21,12 @@
      city: city,
      zip: zip
    }
-<<<<<<< HEAD
-  //  donors.push(obj)
-  console.log(obj)
-   donorList(obj);
 
-=======
    donors.push(obj)
    console.log(obj)
    document.getElementById('donorForm').reset();
    donorList();
-   
->>>>>>> bd7d3fee3da822dc2a0e535784dbd844142469c8
+
  }
 //  const validateForm = () => {
 //   let x = document.forms["add_new_donor"]["firstname"].value;
@@ -40,19 +34,13 @@
 //   if(x == ''){
 //     alert("First Name is Required!")
 //   }
-<<<<<<< HEAD
 
 //  }
- const donorList = (obj) => {
-=======
-  
-//  } 
  const donorList = () => {
    let current = donors[donors.length-1];
->>>>>>> bd7d3fee3da822dc2a0e535784dbd844142469c8
    let textVal = document.createElement('li');
    textVal.innerHTML = current.first + ' ' + current.last + ' Email: ' + current.email + ' Group: ' + current.org + ' Location: ' + current.city + ' Zip: ' + current.zip
    document.getElementById('donorList').appendChild(textVal)
-   console.log(donors) 
+   console.log(donors)
  }
  document.getElementById('submit').addEventListener('click', testing)
